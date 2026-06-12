dd636a0b4f979e3a0fdbe59f66aeec5ffbb526e5
ralphilius <4253551+ralphilius@users.noreply.github.com>
2026-06-12 10:33:40 +0700

    feat: add normalized subscription event API and Paddle mapping (#4)

--- src/lib/askrift.ts
+++ src/lib/askrift.ts
@@ -1,49 +1,78 @@
-import { VercelRequest } from "@vercel/node";
-import { Request } from 'express';
-import { 
-  SubscriptionCancelled, 
-  SubscriptionCreated, 
-  SubscriptionPaymentFailed, 
-  SubscriptionPaymentRefunded, 
-  SubscriptionPaymentSucceeded, 
-  SubscriptionUpdated } from "../types/paddle/subscription";
+import {
+  NormalizedEventByType,
+  NormalizedSubscriptionEvent,
+  SUBSCRIPTION_EVENT_TYPES,
+  SubscriptionEventType,
+} from "../types/events";
 
-export default abstract class Askrift<SC extends keyof TypesMap> implements ISubscriptionCreated<SC> {
+type ProviderEventMap = Partial<Record<SubscriptionEventType, unknown>>;
+
+type ProviderEvents<Events> = Events extends string
+  ? Record<SubscriptionEventType, any>
+  : Events extends ProviderEventMap
+    ? Events
+    : Record<SubscriptionEventType, NormalizedSubscriptionEvent>;
+
+type ProviderEventPayload<Events, TType extends SubscriptionEventType> =
+  TType extends keyof ProviderEvents<Events> ? ProviderEvents<Events>[TType] : NormalizedEventByType<TType>;
+
+export default abstract class Askrift<Events extends ProviderEventMap | string = Record<SubscriptionEventType, NormalizedSubscriptionEvent>> {
   private _debug: boolean;
-  constructor(debug?: boolean){
+
+  constructor(debug?: boolean) {
     this._debug = debug || false;
   }
-  abstract onSubscriptionCanceled(): Promise<TypesMap[SC]["canceled"]>;
-  abstract onSubscriptionUpdated(): Promise<TypesMap[SC]["updated"]>;
-  abstract onPaymentSucceeded(): Promise<TypesMap[SC]["paymentSucceeded"]>;
-  abstract onPaymentFailed(): Promise<TypesMap[SC]["paymentFailed"]>;
-  abstract onPaymentRefunded(): Promise<TypesMap[SC]["paymentRefunded"]>;
-  abstract onSubscriptionCreated(): Promise<TypesMap[SC]['created']>;
 
-  public debug(msg: any, ...optionalParams: any[]){
-    if(this._debug) console.log(msg, ...optionalParams);
+  public debug(msg: any, ...optionalParams: any[]) {
+    if (this._debug) console.log(msg, ...optionalParams);
   }
 
   abstract validRequest(): boolean;
-  abstract validPayload(): boolean;
-}
 
-export type TypesMap = {
-  paddle: {
-    created: SubscriptionCreated | null
-    updated: SubscriptionUpdated | null
-    paymentSucceeded: SubscriptionPaymentSucceeded | null
-    paymentFailed: SubscriptionPaymentFailed | null
-    paymentRefunded: SubscriptionPaymentRefunded | null
-    canceled: SubscriptionCancelled | null
+  validPayload(): boolean {
+    return this.verify();
   }
-}
 
-interface ISubscriptionCreated<Vendor extends keyof TypesMap> {
-  onSubscriptionCreated(): Promise<TypesMap[Vendor]['created']>
-  onSubscriptionCanceled(): Promise<TypesMap[Vendor]['canceled']>
-  onSubscriptionUpdated(): Promise<TypesMap[Vendor]['updated']>
-  onPaymentSucceeded(): Promise<TypesMap[Vendor]['paymentSucceeded']>;
-  onPaymentFailed(): Promise<TypesMap[Vendor]['paymentFailed']>;
-  onPaymentRefunded(): Promise<TypesMap[Vendor]['paymentRefunded']>;
+  abstract verify(): boolean;
+  abstract getEventType(): SubscriptionEventType | null;
+  abstract toNormalizedEvent(): NormalizedSubscriptionEvent | null;
+
+  parseEvent(): Promise<NormalizedSubscriptionEvent | null> {
+    if (!this.verify()) {
+      return Promise.resolve(null);
+    }
+    return Promise.resolve(this.toNormalizedEvent());
+  }
+
+  protected async getProviderEvent<TType extends SubscriptionEventType>(
+    type: TType,
+  ): Promise<ProviderEventPayload<Events, TType> | null> {
+    const event = await this.parseEvent();
+    if (event?.type !== type) return null;
+    return event.raw as ProviderEventPayload<Events, TType>;
+  }
+
+  onSubscriptionCreated(): Promise<ProviderEventPayload<Events, typeof SUBSCRIPTION_EVENT_TYPES.SubscriptionCreated> | null> {
+    return this.getProviderEvent(SUBSCRIPTION_EVENT_TYPES.SubscriptionCreated);
+  }
+
+  onSubscriptionCanceled(): Promise<ProviderEventPayload<Events, typeof SUBSCRIPTION_EVENT_TYPES.SubscriptionCancelled> | null> {
+    return this.getProviderEvent(SUBSCRIPTION_EVENT_TYPES.SubscriptionCancelled);
+  }
+
+  onSubscriptionUpdated(): Promise<ProviderEventPayload<Events, typeof SUBSCRIPTION_EVENT_TYPES.SubscriptionUpdated> | null> {
+    return this.getProviderEvent(SUBSCRIPTION_EVENT_TYPES.SubscriptionUpdated);
+  }
+
+  onPaymentSucceeded(): Promise<ProviderEventPayload<Events, typeof SUBSCRIPTION_EVENT_TYPES.PaymentSucceeded> | null> {
+    return this.getProviderEvent(SUBSCRIPTION_EVENT_TYPES.PaymentSucceeded);
+  }
+
+  onPaymentFailed(): Promise<ProviderEventPayload<Events, typeof SUBSCRIPTION_EVENT_TYPES.PaymentFailed> | null> {
+    return this.getProviderEvent(SUBSCRIPTION_EVENT_TYPES.PaymentFailed);
+  }
+
+  onPaymentRefunded(): Promise<ProviderEventPayload<Events, typeof SUBSCRIPTION_EVENT_TYPES.PaymentRefunded> | null> {
+    return this.getProviderEvent(SUBSCRIPTION_EVENT_TYPES.PaymentRefunded);
+  }
 }
--- src/lib/paddle.ts
+++ src/lib/paddle.ts
@@ -3,10 +3,40 @@ import { Request } from 'express';
 import * as crypto from "crypto";
 import { serialize } from 'php-serialize';
 import Askrift from "./askrift";
-import { SubscriptionCancelled, SubscriptionCreated, SubscriptionPaymentFailed, SubscriptionPaymentRefunded, SubscriptionPaymentSucceeded, SubscriptionUpdated } from "../types/paddle/subscription";
+import {
+  SubscriptionCancelled,
+  SubscriptionCreated,
+  SubscriptionPaymentFailed,
+  SubscriptionPaymentRefunded,
+  SubscriptionPaymentSucceeded,
+  SubscriptionUpdated,
+} from "../types/paddle/subscription";
+import {
+  NormalizedSubscriptionEvent,
+  SUBSCRIPTION_EVENT_TYPES,
+  SubscriptionEventType,
+} from "../types/events";
 import { isObject } from "./utils";
 
 type PaddlePayload = { [k: string]: any };
+
+export type PaddleSubscriptionEvents = {
+  [SUBSCRIPTION_EVENT_TYPES.SubscriptionCreated]: SubscriptionCreated;
+  [SUBSCRIPTION_EVENT_TYPES.SubscriptionUpdated]: SubscriptionUpdated;
+  [SUBSCRIPTION_EVENT_TYPES.SubscriptionCancelled]: SubscriptionCancelled;
+  [SUBSCRIPTION_EVENT_TYPES.PaymentSucceeded]: SubscriptionPaymentSucceeded;
+  [SUBSCRIPTION_EVENT_TYPES.PaymentFailed]: SubscriptionPaymentFailed;
+  [SUBSCRIPTION_EVENT_TYPES.PaymentRefunded]: SubscriptionPaymentRefunded;
+};
+
+const PADDLE_EVENT_TYPES: Record<string, SubscriptionEventType> = {
+  subscription_created: SUBSCRIPTION_EVENT_TYPES.SubscriptionCreated,
+  subscription_updated: SUBSCRIPTION_EVENT_TYPES.SubscriptionUpdated,
+  subscription_cancelled: SUBSCRIPTION_EVENT_TYPES.SubscriptionCancelled,
+  subscription_payment_succeeded: SUBSCRIPTION_EVENT_TYPES.PaymentSucceeded,
+  subscription_payment_failed: SUBSCRIPTION_EVENT_TYPES.PaymentFailed,
+  subscription_payment_refunded: SUBSCRIPTION_EVENT_TYPES.PaymentRefunded,
+};
 
 function ksort(obj: PaddlePayload) {
   const keys = Object.keys(obj).sort();
@@ -15,6 +45,21 @@ function ksort(obj: PaddlePayload) {
     sortedObj[keys[i]] = obj[keys[i]];
   }
   return sortedObj;
+}
+
+function toDate(value?: Date | string): Date | undefined {
+  if (!value) return undefined;
+  if (value instanceof Date) return value;
+  const normalized = typeof value === 'string' && value.includes(' ')
+    ? value.replace(' ', 'T') + 'Z'
+    : value;
+  const date = new Date(normalized);
+  return isNaN(date.getTime()) ? undefined : date;
+}
+
+function normaliseContentType(contentType: string | string[] | undefined): string {
+  const value = Array.isArray(contentType) ? contentType[0] : contentType;
+  return value?.split(';')[0].trim().toLowerCase() || '';
 }
 
 function parseBody(body: any): PaddlePayload | null {
@@ -36,98 +81,181 @@ function parseBody(body: any): PaddlePayload | null {
   }
 }
 
-function normalizeContentType(contentType: string | string[] | undefined): string {
-  const value = Array.isArray(contentType) ? contentType[0] : contentType;
-  return value?.split(';')[0].trim().toLowerCase() || '';
-}
-
-function promisify<T>(obj: any, key: string): Promise<T | null> {
-  return new Promise<T | null>((resolve, reject) => {
-    const payload = parseBody(obj);
-    if (payload) {
-      if (payload['alert_name'] == key) {
-        resolve(payload as T);
-      } else {
-        resolve(null)
-      }
-    } else {
-      reject("Invalid body")
-    }
-  });
-}
-
-export default class Paddle extends Askrift<"paddle"> {
+export default class Paddle extends Askrift<PaddleSubscriptionEvents> {
   private _req;
   private _pubKey: string;
+  private _parsedBody: PaddlePayload | null = null;
+  private _parsedEventPromise: Promise<NormalizedSubscriptionEvent | null> | null = null;
+
   constructor(req: VercelRequest | Request, debugged?: boolean) {
     super(debugged);
-    if (!process.env.PADDLE_PUBLIC_KEY) throw "PADDLE_PUBLIC_KEY is required";
+    if (!process.env.PADDLE_PUBLIC_KEY) throw new Error("PADDLE_PUBLIC_KEY is required");
     this._req = req;
-    this._pubKey = `-----BEGIN PUBLIC KEY-----\n${process.env.PADDLE_PUBLIC_KEY?.replace(/\\n/g, '\n')}\n-----END PUBLIC KEY-----`
+    this._pubKey = `-----BEGIN PUBLIC KEY-----\n${process.env.PADDLE_PUBLIC_KEY?.replace(/\\n/g, '\n')}\n-----END PUBLIC KEY-----`;
   }
 
   onSubscriptionCreated(): Promise<SubscriptionCreated | null> {
-    return promisify(this._req.body, 'subscription_created');
+    return this.getProviderEvent(SUBSCRIPTION_EVENT_TYPES.SubscriptionCreated);
   }
 
   onSubscriptionCanceled(): Promise<SubscriptionCancelled | null> {
-    return promisify(this._req.body, 'subscription_cancelled');
+    return this.getProviderEvent(SUBSCRIPTION_EVENT_TYPES.SubscriptionCancelled);
   }
 
   onSubscriptionUpdated(): Promise<SubscriptionUpdated | null> {
-    return promisify(this._req.body, 'subscription_updated');
+    return this.getProviderEvent(SUBSCRIPTION_EVENT_TYPES.SubscriptionUpdated);
   }
 
   onPaymentSucceeded(): Promise<SubscriptionPaymentSucceeded | null> {
-    return promisify(this._req.body, 'subscription_payment_succeeded');
+    return this.getProviderEvent(SUBSCRIPTION_EVENT_TYPES.PaymentSucceeded);
   }
 
   onPaymentFailed(): Promise<SubscriptionPaymentFailed | null> {
-    return promisify(this._req.body, 'subscription_payment_failed');
+    return this.getProviderEvent(SUBSCRIPTION_EVENT_TYPES.PaymentFailed);
   }
-  
+
   onPaymentRefunded(): Promise<SubscriptionPaymentRefunded | null> {
-    return promisify(this._req.body, 'subscription_payment_refunded');
+    return this.getProviderEvent(SUBSCRIPTION_EVENT_TYPES.PaymentRefunded);
   }
 
   validRequest(): boolean {
-    return this._req.method == 'POST' && normalizeContentType(this._req.headers['content-type']) == 'application/x-www-form-urlencoded';
+    return this._req.method == 'POST' && normaliseContentType(this._req.headers['content-type']) == 'application/x-www-form-urlencoded';
   }
 
-  validPayload(): boolean {
+  verify(): boolean {
     this.debug(this._req.body);
-    const payload = parseBody(this._req.body);
-    if (!payload || typeof payload.p_signature !== 'string') return false;
-    if (typeof this._req.body === 'string') {
-      this._req.body = payload;
+    const body = parseBody(this._req.body);
+    if (!body || typeof body.p_signature !== 'string') {
+      this._parsedBody = null;
+      return false;
     }
 
     this.debug("PADDLE_PUBLIC_KEY", this._pubKey);
-    // Keep the original request body intact for event handlers while verifying
-    // a copy without p_signature, as required by Paddle.
-    const { p_signature, ...unsignedPayload } = payload;
+    const { p_signature, ...unsignedPayload } = body;
     let jsonObj = ksort(unsignedPayload);
     for (let property in jsonObj) {
       if (jsonObj.hasOwnProperty(property) && (typeof jsonObj[property]) !== "string") {
-        if (Array.isArray(jsonObj[property])) { // is it an array
+        if (Array.isArray(jsonObj[property])) {
           jsonObj[property] = jsonObj[property].toString();
-        } else { //if its not an array and not a string, then it is a JSON obj
+        } else {
           jsonObj[property] = JSON.stringify(jsonObj[property]);
         }
       }
     }
-    // Serialise remaining fields of jsonObj
-    const serialized = serialize(jsonObj);
-    const verifier = crypto.createVerify('sha1');
-    verifier.update(serialized);
-    verifier.end();
 
     try {
+      const serialized = serialize(jsonObj);
+      const verifier = crypto.createVerify('sha1');
+      verifier.update(serialized);
+      verifier.end();
+
       const mySig = Buffer.from(p_signature, 'base64');
-      return verifier.verify(this._pubKey, mySig);
+      const verified = verifier.verify(this._pubKey, mySig);
+      if (verified) {
+        this._parsedBody = body;
+      } else {
+        this._parsedBody = null;
+      }
+      return verified;
     } catch (error) {
       this.debug(error);
+      this._parsedBody = null;
       return false;
     }
+  }
+
+  getEventType(): SubscriptionEventType | null {
+    const body = this._parsedBody;
+    if (!body || typeof body.alert_name !== 'string') return null;
+    return PADDLE_EVENT_TYPES[body.alert_name] || null;
+  }
+
+  toNormalizedEvent(): NormalizedSubscriptionEvent | null {
+    const body = this._parsedBody;
+    if (!body) return null;
+
+    const type = this.getEventType();
+    if (!type) return null;
+
+    const base = {
+      type,
+      provider: 'paddle',
+      raw: body,
+      eventId: body.alert_id,
+      occurredAt: toDate(body.event_time),
+      subscriptionId: body.subscription_id,
+      subscriptionPlanId: body.subscription_plan_id,
+      customerId: body.user_id,
+      customerEmail: body.email,
+      currency: body.currency,
+      status: body.status,
+    };
+
+    switch (type) {
+      case SUBSCRIPTION_EVENT_TYPES.SubscriptionCreated:
+        return {
+          ...base,
+          type,
+          nextBillDate: toDate(body.next_bill_date),
+        };
+      case SUBSCRIPTION_EVENT_TYPES.SubscriptionUpdated:
+        return {
+          ...base,
+          type,
+          nextBillDate: toDate(body.next_bill_date),
+          previousStatus: body.old_status,
+          previousSubscriptionPlanId: body.old_subscription_plan_id,
+        };
+      case SUBSCRIPTION_EVENT_TYPES.SubscriptionCancelled:
+        return {
+          ...base,
+          type,
+          cancellationEffectiveDate: toDate(body.cancellation_effective_date),
+        };
+      case SUBSCRIPTION_EVENT_TYPES.PaymentSucceeded:
+        return {
+          ...base,
+          type,
+          paymentId: body.subscription_payment_id,
+          orderId: body.order_id,
+          amount: body.sale_gross,
+          nextBillDate: toDate(body.next_bill_date),
+          receiptUrl: body.receipt_url,
+        };
+      case SUBSCRIPTION_EVENT_TYPES.PaymentFailed:
+        return {
+          ...base,
+          type,
+          paymentId: body.subscription_payment_id,
+          orderId: body.order_id,
+          amount: body.amount,
+          nextRetryDate: toDate(body.next_retry_date),
+          attemptNumber: body.attempt_number,
+        };
+      case SUBSCRIPTION_EVENT_TYPES.PaymentRefunded:
+        return {
+          ...base,
+          type,
+          paymentId: body.subscription_payment_id,
+          orderId: body.order_id,
+          amount: body.amount,
+          refundType: body.refund_type,
+          refundReason: body.refund_reason,
+        };
+      default:
+        return null;
+    }
+  }
+
+  async parseEvent(): Promise<NormalizedSubscriptionEvent | null> {
+    if (this._parsedEventPromise) {
+      return this._parsedEventPromise;
+    }
+    if (!parseBody(this._req.body)) {
+      throw new Error("Could not parse webhook body");
+    }
+    const result = this.verify() ? this.toNormalizedEvent() : null;
+    this._parsedEventPromise = Promise.resolve(result);
+    return this._parsedEventPromise;
   }
 }
